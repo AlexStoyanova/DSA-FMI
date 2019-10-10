d2584932f25996ff5c9dfbfdccd04a3d3508e37b
AlexStoyanova <al.stoqnova74@gmail.com>
2019-10-10 21:58:36 +0300

    Add template stack

---
+++ Stack_DSA/Stack_DSA/Stack.h
@@ -0,0 +1,137 @@
+#ifndef STACK_H
+#define STACK_H
+
+template <typename T>
+class Stack
+{
+private:
+	size_t capacity;
+	size_t top;
+	T* data;
+
+private:
+	void clear();
+	void copyFrom(const Stack<T>& other);
+	void resize();
+
+public:
+	Stack();
+	Stack(size_t newCapacity);
+	Stack(const Stack<T>& other);
+	Stack<T>& operator=(const Stack<T>& rhs);
+	~Stack();
+
+	bool isEmpty() const;
+	void push(const T& member);
+	void pop();
+	const T& peek() const;
+	size_t size() const;
+};
+
+#endif
+
+template<typename T>
+inline void Stack<T>::clear()
+{
+	delete[] data;
+}
+
+template<typename T>
+inline void Stack<T>::copyFrom(const Stack<T>& other)
+{
+	top = other.top;
+	capacity = other.capacity;
+	for (size_t i = 0; i < top; ++i)
+	{
+		data[i] = other.data[i];
+	}
+}
+
+template<typename T>
+inline void Stack<T>::resize()
+{
+	T* buff;
+	buff = data;
+	data = new T[2 * capacity];
+	for (size_t i = 0; i < top; ++i)
+	{
+		data[i] = buff[i];
+	}
+	delete[] buff;
+	capacity *= 2;
+}
+
+template<typename T>
+inline Stack<T>::Stack() : capacity(0), top(0), data(nullptr)
+{}
+
+template<typename T>
+inline Stack<T>::Stack(size_t newCapacity) : capacity(newCapacity), top(0)
+{
+	data = new T[capacity];
+}
+
+template<typename T>
+inline Stack<T>::Stack(const Stack<T>& other)
+{
+	copyFrom(other);
+}
+
+template<typename T>
+inline Stack<T>& Stack<T>::operator=(const Stack<T>& rhs)
+{
+	if (this != &rhs)
+	{
+		copyFrom(rhs);
+	}
+	return *this;
+}
+
+template<typename T>
+inline Stack<T>::~Stack()
+{
+	clear();
+}
+
+
+template<typename T>
+inline bool Stack<T>::isEmpty() const
+{
+	return !top;
+}
+
+template<typename T>
+inline void Stack<T>::push(const T & member)
+{
+	if (top == capacity)
+	{
+		resize();
+	}
+	data[top++] = member;
+}
+
+template<typename T>
+inline void Stack<T>::pop()
+{
+	if (!isEmpty())
+	{
+		top--;
+	}
+}
+
+template<typename T>
+inline const T & Stack<T>::peek() const
+{
+	if (isEmpty())
+	{
+		std::cout << "The stack is empty!!!" << std::endl;
+		return 0;                                                  //must be changed
+	}
+	return data[top - 1];
+}
+
+template<typename T>
+inline size_t Stack<T>::size() const
+{
+	return top;
+}
---
+++ Stack_DSA/Stack_DSA/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Stack.h"
+
+int main()
+{
+	Stack<int> stack(6);
+	stack.push(1);
+	stack.push(5);
+	std::cout << stack.peek() << std::endl;
+	stack.pop();
+	std::cout << stack.peek() << std::endl;
+	std::cout << stack.size() << std::endl;
+
+
+	return 0;
+}
